7773d01462931670b526a0a761ddcddd7922f9b4
sushmithaLakma <62799813+sushmithaLakma@users.noreply.github.com>
2021-07-11 01:07:43 +0530

    Update car.py

--- car.py
+++ car.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly
 import pickle
 from sklearn.model_selection import train_test_split
 #from sklearn.linear_model import LinearRegression
